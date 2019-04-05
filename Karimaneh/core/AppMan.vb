32dab9500641c4907d3e8d0e48fcad4ec6fc280f
PurTahan <sina.farahabadi@gmail.com>
2019-04-05 08:02:04 +0430

    افزودن جدول ها و اینام های وابسطه

--- Karimaneh/core/AppMan.vb
+++ Karimaneh/core/AppMan.vb
@@ -46,6 +46,319 @@ Public Class AppMan
                 SQLiter.RunCommand("create table IF NOT EXISTS tStarts(ID INTEGER PRIMARY KEY AUTOINCREMENT,TT text, SS text)")
             End If
 
+            If dbVer = 1 Then
+                SQLiter.RunCommand("create table IF NOT EXISTS tAfrad
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Esm text,
+                    Mostaar text,
+                    Famil text,
+                    Pedar text,
+                    A_Seyed INTEGER,
+                    Jens INTEGER,
+                    CM INTEGER,
+                    CMPedar INTEGER,
+                    TT INTEGER,
+                    ST INTEGER,
+                    A_TD INTEGER,
+                    Taahol INTEGER,
+                    A_AstanehEzdevaj INTEGER,
+                    A_Shaqel INTEGER,
+                    A_BimarKhas INTEGER,
+                    A_TahteBimeh INTEGER,
+                    A_Motad INTEGER,
+                    A_MadadJu INTEGER,
+                    Hayat INTEGER,
+                    TVafat INTEGER,
+                    A_Malul INTEGER,
+                    Din INTEGER,
+                    Mazhab INTEGER,
+                    A_DD INTEGER,
+                    Mob1 text,
+                    Mob2 text,
+                    Mob3 text,
+                    JamDaramadHa INTEGER,
+                    A_Hozavi INTEGER,
+                    Tahsilat INTEGER,
+                    ReshtehTahsili text,
+                    KhanehVar INTEGER,
+                    Tozih text,
+                    Vaziat INTEGER,
+                    K_Sabt INTEGER,
+                    ZSabt INTEGER,
+                    K_Virayesh INTEGER,
+                    ZVirayesh INTEGER,
+                    T_Virayesh INTEGER,
+                    K_Taeed INTEGER,
+                    ZTaeed INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhanehVar
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Sarparast INTEGER,
+                    Maskan INTEGER,
+                    NoMaskan INTEGER,
+                    A_MaskanNaSalem INTEGER,
+                    A_MaskanNaMonaseb INTEGER,
+                    A_MadadJu INTEGER,
+                    Tozih text,
+                    Moarref INTEGER,
+                    Rabet INTEGER,
+                    Vaziat INTEGER,
+                    K_Sabt INTEGER,
+                    ZSabt INTEGER,
+                    K_Virayesh INTEGER,
+                    ZVirayesh INTEGER,
+                    T_Virayesh INTEGER,
+                    K_Taeed INTEGER,
+                    ZTaeed INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tBimariHayeKhas
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text,
+                    A_HemayatDolati INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tBimariKhas
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Bimari INTEGER,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    A_Darman INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tEtiadHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tEtiad
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Etiad INTEGER,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    A_Darman INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMaluliatHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tEtiad
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Maluliat INTEGER,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    A_Darman INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tBimehHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tBimeh
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Bimeh INTEGER,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tHamRastaHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text,
+                    Tamas1 text,
+                    Tamas2 text,
+                    Tamas3 text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tHamRasta
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    KhanehVar INTEGER,
+                    HamRasta INTEGER,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMaharatHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMaharat
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Maharat INTEGER,
+                    Sath INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tTajarobKari
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Onvan text,
+                    Tozih text,
+                    AzT INTEGER,
+                    TaT INTEGER,
+                    DalilPayan text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMazhabHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Din INTEGER,
+                    Tozih text
+                    )")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('نا معلوم','{Enums.DinHa.NaMalum}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('شیعه','{Enums.DinHa.Eslam}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('سنی','{Enums.DinHa.Eslam}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('وهابی','{Enums.DinHa.Eslam}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('نا معلوم','{Enums.DinHa.Eslam}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('نا معلوم','{Enums.DinHa.Masihi}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('نا معلوم','{Enums.DinHa.Yahudi}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('نا معلوم','{Enums.DinHa.Zartoshti}')")
+                SQLiter.RunCommand($"insert into tMazhabHa(Onvan,Din) values('بهائی','{Enums.DinHa.FerqeHayeZalleh}')")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMakan
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Radeh INTEGER,
+                    Onvan text,
+                    Noqat text,
+                    Markaz text,
+                    Adres text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tMakanVahed
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Makan INTEGER,
+                    Tabaqeh INTEGER,
+                    Vahed INTEGER,
+                    Tel1 text,
+                    Tel2 text,
+                    Posti INTEGER,
+                    Metraj INTEGER,
+                    T_Khab INTEGER,
+                    A_Ab INTEGER,
+                    A_AbGarm INTEGER,
+                    A_Barq INTEGER,
+                    A_Gaz INTEGER,
+                    A_Tualet INTEGER,
+                    A_Hammam INTEGER,
+                    Tozih text,
+                    A_Daqiq INTEGER,
+                    K_Sabt INTEGER,
+                    ZSabt INTEGER,
+                    K_Virayesh INTEGER,
+                    ZVirayesh INTEGER,
+                    T_Virayesh INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tAnvaMaskan
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text,
+                    A_Shakhsi INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tYaddashtHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Mohtava text,
+                    MarbutBe INTEGER,
+                    Bakhsh INTEGER,
+                    K_Sabt INTEGER,
+                    ZSabt INTEGER,
+                    K_Virayesh INTEGER,
+                    ZVirayesh INTEGER,
+                    T_Virayesh INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tAnvaKhedmat
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Onvan text,
+                    Tozih text,
+                    A_Fardi INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhedmatFardiNiaz
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Khedmat INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhedmatKhNiaz
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    KhanehVar INTEGER,
+                    Khedmat INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhedmatFardiShodeh
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Fard INTEGER,
+                    Khedmat INTEGER,
+                    Tozih text,
+                    Tarikh INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhedmatKhShodehHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Khedmat INTEGER,
+                    Tozih text,
+                    Tarikh INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKhedmatKhShodeh
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    KhanehVar INTEGER,
+                    KhedmatKhShodeh INTEGER,
+                    Tozih text
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tKarbarHa
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    kUser text,
+                    kPass text,
+                    Onvan text,
+                    Mob text,
+                    A_ModirKol INTEGER,
+                    Tozih text,
+                    DastResiHa text,
+                    ZIjad text,
+                    TAVorud INTEGER,
+                    SAVorud INTEGER
+                    )")
+
+                SQLiter.RunCommand("create table IF NOT EXISTS tTaqirat
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Karbar INTEGER,
+                    Tarikh INTEGER,
+                    Saat INTEGER,
+                    NoTaqir INTEGER,
+                    Tozih text,
+                    A_ChekShodeh INTEGER,
+                    ModirChek INTEGER
+                    )")
+
+
+                Tanzimat("dbVer") = 2
+            End If
+
             'If dbVer = 1 Then
             '    Tanzimat("dbVer") = 1000
             'End If
@@ -66,8 +379,7 @@ Public Class AppMan
         New SQLiteParameter("@0", key),
         New SQLiteParameter("@1", value)
                                   }
-            If SQLiter.RunCommand("update tTanzimat set Meqdar=@1 where Onvan like @0", prms
-) = 0 Then
+            If SQLiter.RunCommand("update tTanzimat set Meqdar=@1 where Onvan like @0", prms) = 0 Then
                 SQLiter.RunCommand("insert into tTanzimat(Onvan,Meqdar) values(@0,@1)", prms)
             End If
         End Set
---
+++ Karimaneh/core/Enums.vb
@@ -0,0 +1,118 @@
+﻿Public Module Enums
+#Region "Omumi"
+    Public Enum BalehKheyr
+        Baleh = 1
+        Kheyr = 0
+    End Enum
+
+    Public Enum BakhshHa
+        Fard = 1
+        KhanehVar = 2
+        Maskan = 3
+        MaskanVahed = 4
+    End Enum
+
+    Public Enum Taqirat_No
+        Ezafeh = 1
+        Hazf = 2
+        Virayesh = 3
+
+        PoshtibanGiri = 11
+
+        Chap = 21
+        KhorujiDadeh = 22
+
+        VorudKarbar = 31
+        KhorujKarbar = 32
+        TaqirKarbar = 33
+
+        KarbarEzafeh = 41
+        KarbarHazf = 42
+        KarbarVirayesh = 43
+        Tanzimat = 44
+    End Enum
+#End Region
+
+#Region "Afrad"
+    Public Enum Afrad_Jens
+        NaMalum = -1
+        Mozakkar = 1
+        Moannas = 2
+        DoJenseh = 3
+    End Enum
+
+    Public Enum Afrad_Taahol
+        NaMalum = -1
+        Mojarrad = 1
+        Moteahel = 2
+        TalaqGerefteh = 3
+        HamsarMordeh = 4
+    End Enum
+
+    Public Enum Afrad_Vaziat
+        Arshiv = -1
+        TaeedNashodeh = 0
+        TaeedShodeh = 1
+    End Enum
+
+    Public Enum DinHa
+        NaMalum = -1
+        Eslam = 1
+        Masihi = 2
+        Yahudi = 3
+        Zartoshti = 4
+
+        FerqeHayeZalleh = 99
+    End Enum
+
+    Public Enum Afrad_Tahsilat
+        NaMalum = -1
+        BiSavad = 0
+        Ebtedaee = 1
+        Rahnamaee = 2
+        Dabirestan = 3
+        Diplom = 4
+        Kardani = 5
+        Karshenasi = 6
+        KarshenasiArshad = 7
+        Doktora = 8
+
+        Sath1 = 21
+        Sath2 = 22
+        Sath3 = 23
+        Sath4 = 24
+        Kharej = 25
+
+        Qeyreh = 99
+    End Enum
+#End Region
+
+#Region "KhanehVar"
+    Public Enum KhanehVar_Vaziat
+        Arshiv = -1
+        TaeedNashodeh = 0
+        TaeedShodeh = 1
+    End Enum
+#End Region
+
+#Region "Maharat"
+    Public Enum Maharat_Sath
+        NaMalum = -1
+        Moqaddamati = 1
+        Mamuli = 2
+        Herfehei = 3
+    End Enum
+#End Region
+
+#Region "Makan"
+    Public Enum Makan_Radeh
+        Ostan = 1
+        Shahrestan = 2
+        Shahr = 3
+        Rusta = 4
+        Mahalleh = 5
+        Melk = 6
+    End Enum
+#End Region
+
+End Module
