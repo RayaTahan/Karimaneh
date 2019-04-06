eb8a31dd905c8eb9bb9b55112e3d5c1710bf6dac
PurTahan <sina.farahabadi@gmail.com>
2019-04-06 09:10:56 +0430

    افزودن تصویر

--- Karimaneh/My Project/Resources.Designer.vb
+++ Karimaneh/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property _22() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("_22", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property bg01() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("bg01", resourceCulture)
